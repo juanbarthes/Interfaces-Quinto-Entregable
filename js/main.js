fde5aafff9ddf1274ec72c6cee81b7c1769a9c5c
Federico Fuhr <46513550+federicofuhr@users.noreply.github.com>
2021-11-29 23:59:53 -0300

    agregada comprobacion del login

--- js/main.js
+++ js/main.js
@@ -1,30 +1,56 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    'use strict';
 
+    /**
+     * Vars
+     */
+    let loginForm = document.querySelector("#login-form");
     let loading = document.querySelector("#loading-section");
     let btnLoading = document.querySelector("#login-submit");
+    let emailError = document.querySelector("#email-error-info");
+    let passwordError = document.querySelector("#password-error-info");
+    let email = document.querySelector("#email");
+    let password = document.querySelector("#password");
 
-    btnLoading.addEventListener('click', function (e) {
-        console.log("entro");
+
+    /**
+     * Events
+     */
+    loginForm.addEventListener('submit', function (e) {
+        if (!passwordError.classList.contains('hidden')) {
+            passwordError.classList.add('hidden');
+        }
         e.preventDefault();
         login(e);
     });
 
+
+    /**
+     * Functions 
+     */
+
+
     function login(e) {
-        let email = document.querySelector("#email");
-        let pwd = document.querySelector("#password");
         btnLoading.disabled = true;
         email.disabled = true;
-        pwd.disabled = true;
+        password.disabled = true;
         loading.classList.remove('hidden');
         setTimeout(() => {
-            console.log(email.value + " " + pwd.value);
-            loading.classList.add('hidden');
-            email.disabled = false;
-            pwd.disabled = false;
-            btnLoading.disabled = false;
+            console.log(email.value + " " + password.value);
+            if (email.value == "test@test.com.ar" && password.value == "12345678") {
+                loading.classList.add('hidden');
+                window.location = 'feed.html';
+            } else {
+                loading.classList.add('hidden');
+                email.disabled = false;
+                password.disabled = false;
+                btnLoading.disabled = false;
+                email.value = "";
+                password.value = "";
+                passwordError.innerHTML = "Las credenciales no coinciden."
+                passwordError.classList.remove('hidden');
+            }
         }, 1000);
     }
 }
