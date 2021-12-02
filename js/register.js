ba549c30c8d6f1f8fae4b7178095c1ce42791d68
Federico Fuhr <46513550+federicofuhr@users.noreply.github.com>
2021-12-02 00:34:52 -0300

    agregadas verificaciones en registro

--- js/register.js
+++ js/register.js
@@ -8,9 +8,14 @@ function init() {
     let registerForm = document.querySelector("#register-form");
     let loading = document.querySelector("#loading-section");
     let btnRegister = document.querySelector("#register-submit");
-    let passwordError = document.querySelector("#password-error-info");
+    let emailError = document.querySelector("#email-error");
+    let passwordError = document.querySelector("#password-error");
     let email = document.querySelector("#email");
+    let registerError = document.querySelector("#register-error");
     let password = document.querySelector("#password");
+    let passwordRepeat = document.querySelector("#password-repeat");
+    let passwordRepeatError = document.querySelector("#password-repeat-error");
+    let genero = document.querySelector("#radio-section");
 
 
     /**
@@ -31,24 +36,39 @@ function init() {
 
 
     function register(e) {
-        btnLoading.disabled = true;
+        genero.disabled = true;
+        btnRegister.disabled = true;
         email.disabled = true;
         password.disabled = true;
         loading.classList.remove('hidden');
         setTimeout(() => {
-            console.log(email.value + " " + password.value);
-            if (email.value == "test@test.com.ar" && password.value == "12345678") {
+            if (email.value == "test@test.com.ar") {
                 loading.classList.add('hidden');
-                window.location = 'feed.html';
-            } else {
-                loading.classList.add('hidden');
+                emailError.innerHTML = "Este correo ya está en uso.";
+                emailError.classList.remove("hidden");
+                btnRegister.disabled = false;
                 email.disabled = false;
                 password.disabled = false;
-                btnLoading.disabled = false;
-                email.value = "";
-                password.value = "";
-                passwordError.innerHTML = "Las credenciales no coinciden."
-                passwordError.classList.remove('hidden');
+            } else {
+                if (password.value != passwordRepeat.value) {
+                    loading.classList.add('hidden');
+                    passwordRepeatError.innerHTML = "Las contraseñas no coinciden.";
+                    passwordRepeatError.classList.remove("hidden");
+                    btnRegister.disabled = false;
+                    email.disabled = false;
+                    password.disabled = false;
+                } else {
+                    email.disabled = false;
+                    password.disabled = false;
+                    btnRegister.disabled = false;
+                    email.value = "";
+                    password.value = "";
+                    registerError.innerHTML = "Cuenta creada satisfactoriamente."
+                    registerError.classList.remove('hidden');
+                    setTimeout(() => {
+                        window.location = 'feed.html';
+                    }, 2000);
+                }
             }
         }, 1000);
     }
